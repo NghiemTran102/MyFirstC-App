8e3399686ce1fd40d4a5aa53b61e81652a7fe22e
Nghiem Tran <nghiem.tran@s3corp.com.vn>
2026-03-16 09:45:50 +0700

    Thuc hanh Git lan 1

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -7,3 +7,4 @@ bool isPro = true;
 Console.WriteLine($"Tay vot: {playerName} \n");
 Console.WriteLine($"So tran thang: {matchesWon} \n");
 Console.WriteLine($"Dung vot hang: {racketBrand} \n");
+Console.WriteLine($"Test Git lan 2 \n");
