e9b2c0ef4bf74bdf5f646a2d102a4dd9cf625ffa
Nghiem Tran <nghiem.tran@s3corp.com.vn>
2026-03-16 09:30:16 +0700

    Khoi tao project C# dau tien va them file gitignore

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿string playerName = "NghiemTran";
+string racketBrand = "Lining";
+int matchesWon = 18;
+double powerScale = 9.5;
+bool isPro = true;
+
+Console.WriteLine($"Tay vot: {playerName} \n");
+Console.WriteLine($"So tran thang: {matchesWon} \n");
+Console.WriteLine($"Dung vot hang: {racketBrand} \n");
